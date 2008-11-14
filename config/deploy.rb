731c7f2763a2e183358109e1123aa077d2de26e2
Eduardo Fernández Corrales <edu@flowersinspace.com>
2008-11-14 14:05:14 +0100

    Redcloth

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ set :ec2onrails_config, {
   # Any extra RubyGems to install if desired: can be "gemname" or if a 
   # particular version is desired "gemname -v 1.0.1"
   # If you don't want to install extra rubygems then remove this
-  :rubygems => ["rmagick", "redcloth", "rfacebook -v 0.9.7"],
+  :rubygems => ["rmagick", "RedCloth", "rfacebook -v 0.9.7"],
 
   # Defines the web proxy that will be used.  Choices are :apache or :nginx
   :web_proxy_server => :apache,
